cc74062fdaf833a4a8ad70842577aaeaf1b9d4ff
Diego Oswaldo Meneses Vera <diegooswaldomeneses@gmail.com>
2026-03-25 14:30:38 -0600

    hicimos unos formularios pero no me jalaron, modificar el html y ts de registro admin y maestros, además del scss para la card blanca

--- src/app/partials/registro-admin/registro-admin.ts
+++ src/app/partials/registro-admin/registro-admin.ts
@@ -1,14 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SHARED_IMPORTS } from '../../shared/shared.imports';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registro-admin',
-  imports: [],
+  imports: [...SHARED_IMPORTS],
   templateUrl: './registro-admin.html',
   styleUrl: './registro-admin.scss',
 })
-export class RegistroAdmin {
+export class RegistroAdmin implements OnInit {
 
   @Input() rol:string = "";
   @Input() datos_user:any = {};
+
+  public admin:any ={};
+  public errors:any = {};
+  public editar:boolean = false;
+  public iduser:any = null;
+
+  constructor(
+    private router: Router,
+    private location: Location
+  ) { }
+
+  ngOnInit() {
+  }
 
 }
--- src/app/partials/registro-maestros/registro-maestros.ts
+++ src/app/partials/registro-maestros/registro-maestros.ts
@@ -1,14 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SHARED_IMPORTS } from '../../shared/shared.imports';
 
 @Component({
   selector: 'app-registro-maestros',
-  imports: [],
+  imports: [...SHARED_IMPORTS],
   templateUrl: './registro-maestros.html',
   styleUrl: './registro-maestros.scss',
 })
-export class RegistroMaestros {
+export class RegistroMaestros implements OnInit {
 
   @Input() rol:string = "";
   @Input() datos_user:any = {};
+
+  constructor() { }
+
+  ngOnInit() {
+  } 
 
 }

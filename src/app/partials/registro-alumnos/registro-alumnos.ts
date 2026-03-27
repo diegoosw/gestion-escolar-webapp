4473414acad76dbeb20d9d8596d81501f8bf0571
Diego Oswaldo Meneses Vera <diegooswaldomeneses@gmail.com>
2026-03-27 14:08:54 -0600

    para la participación del viernes del 26 de marzo de 2026

--- src/app/partials/registro-alumnos/registro-alumnos.ts
+++ src/app/partials/registro-alumnos/registro-alumnos.ts
@@ -1,13 +1,86 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { SHARED_IMPORTS } from '../../shared/shared.imports';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-registro-alumnos',
-  imports: [],
+  imports: [
+    ...SHARED_IMPORTS
+  ],
   templateUrl: './registro-alumnos.html',
   styleUrl: './registro-alumnos.scss',
 })
-export class RegistroAlumnos {
+export class RegistroAlumnos implements OnInit {
+
   @Input() rol:string = "";
   @Input() datos_user:any = {};
-  
+
+  public alumno: any = {};
+  public errors: any = {};
+  public editar:boolean = false;
+  public idUser: number = 0;
+
+  //Para contraseñas
+  public hide_1: boolean = false;
+  public hide_2: boolean = false;
+  public inputType_1: string = 'password';
+  public inputType_2: string = 'password';
+
+  //Para el select
+
+  constructor(
+    private location: Location,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+  }
+
+  //Funciones para password
+  public showPassword()
+  {
+    if(this.inputType_1 === 'password'){
+      this.inputType_1 = 'text';
+      this.hide_1 = true;
+    }
+    else{
+      this.inputType_1 = 'password';
+      this.hide_1 = false;
+    }
+  }
+
+  public showPwdConfirmar()
+  {
+    if(this.inputType_2 === 'password'){
+      this.inputType_2 = 'text';
+      this.hide_2 = true;
+    }
+    else{
+      this.inputType_2 = 'password';
+      this.hide_2 = false;
+    }
+  }
+
+  public regresar(){
+    this.location.back();
+  }
+
+  public registrar(){
+
+  }
+
+  public actualizar(){
+
+  }
+
+  //Función para detectar el cambio de fecha
+  public changeFecha(event :any){
+    this.alumno.fecha_nacimiento = event.value.toISOString().split("T")[0];
+  }
+
+
+
+
+
 }
